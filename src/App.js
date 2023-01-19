431dda60ea14cbf9aa3e79a16aa0424b50252da0
Fulya <fulya.ertay@gmail.com>
2023-01-19 14:30:01 +0300

    added custom hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './index.css';
-import './output.css';
+import useFetchData from './my_hook/custom_hook';
+import Axios from "axios"
 function App() {
+  const [todoFetch, _todoFetch]=useFetchData()
+  async function onGetTodo(){
+    try {
+      _todoFetch.onLoading()
+      const data=await Axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response=>response.data)
+      
+    } catch (error) {
+      
+    }
+  }
   return (
-    <div className="App bg-blue-500">
-      <p>sdjsdj</p>
+    <div className="App">
+      <h1 className='text-center text-2xl mt-5'>Custom Hooks-Fetch Data</h1>
+      <div className='text-center text-xl'>Status</div>
     </div>
   );
 }
--- src/my_hook/custom_hook.js
+++ src/my_hook/custom_hook.js
@@ -34,4 +34,5 @@ export default function useFetchData(){
            }
         )
     }
+    return [fetchData,{onLoading,onError,onSuccess}]
 }
